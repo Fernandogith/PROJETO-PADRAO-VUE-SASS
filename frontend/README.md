56baf29c538977ddd41b87a5c86cf5305282e63e
Fernandogith <fernandosouza.inbox@outlook.com>
2022-10-04 06:48:54 -0300

    Inicio: Padrão Finalizado

--- frontend/README.md
+++ frontend/README.md
@@ -1,11 +1,4 @@
-# PROJETO-PADRAO-VUE
-
-- Padrão para projetos de desenvolvimento com VUE.
-
-- Neste temos a instalação do VUE, com os exemplos que o VUE traz já limpos, basta CLONAR e usar.
-
-https://www.notion.so/Padr-es-VUE-3b5a3e04d4b14405bcdd195cceadee18
-
+# byte-new
 
 ## Project setup
 ```
---
+++ frontend/src/pages/home/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+
+        <h1>Home</h1>
+       
+        <div class="divider" style="padding: 20px"></div>
+
+        <div class="">
+            <button class="btn-primary">btn-primary</button>
+        </div>
+
+        <div class="divider" style="padding: 20px"></div>
+        
+        <div class="">
+            <button class="btn-secondary">.btn-secondary</button>
+        </div>
+
+    </div>
+
+
+</template>
+
+
+<script>
+
+</script>
+
+<style src="./style.scss" lang="scss" scoped>
