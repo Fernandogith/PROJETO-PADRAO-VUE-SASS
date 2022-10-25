ce961241a561adc27925d347d08508662e291271
Fernandogith <fernandosouza.inbox@outlook.com>
2022-10-25 15:08:49 -0300

    Padrão Finalizado

--- frontend/src/pages/home/Home.vue
+++ frontend/src/pages/home/Home.vue
@@ -1,18 +1,21 @@
 <template>
     <div class="container">
-
         <h1>Home</h1>
-       
-        <div class="divider" style="padding: 20px"></div>
 
-        <div class="">
-            <button class="btn-primary">btn-primary</button>
-        </div>
+        <div class="componentes-scss">
+            <h1>Componentes criados com SCSS</h1>
 
-        <div class="divider" style="padding: 20px"></div>
-        
-        <div class="">
-            <button class="btn-secondary">.btn-secondary</button>
+            <div class="divider" style="padding: 20px"></div>
+
+            <div class="">
+                <button class="btn-primary">btn-primary</button>
+            </div>
+
+            <div class="divider" style="padding: 20px"></div>
+            
+            <div class="">
+                <button class="btn-secondary">.btn-secondary</button>
+            </div>
         </div>
 
     </div>
